a32676fae55f41a6a8c06db2fb85f7b8577d01ea
Ollie Smith <o.g.smith@icloud.com>
2023-09-27 22:02:28 +0100

    lint

--- server/index.js
+++ server/index.js
@@ -1,21 +1,19 @@
+const express = require('express')
+const MongoDBConnector = require('./mongo')
 
-const express = require("express");
-const MongoDBConnector = require("./mongo");
+const app = express()
 
-const app = express();
+const PORT = 3001
+const mongoDB = new MongoDBConnector()
 
-const PORT = 3001;
-const mongoDB = new MongoDBConnector() 
+app.get('/api/users', async (req, res) => {
+  try {
+    result = await mongoDB.queryCollection('users', {})
+    return res.status(200).json(result)
+  } catch (error) {
+    console.error('Error:', error)
+    return res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
 
-app.get("/api/users", async (req, res) => {
-    try {
-      result = await mongoDB.queryCollection("users", {});
-      return res.status(200).json(result);
-      
-    } catch (error) {
-        console.error("Error:", error);
-        return res.status(500).json({ error: "Internal Server Error" });
-    }
-});
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
--- server/mongo.js
+++ server/mongo.js
@@ -1,53 +1,50 @@
-const { MongoClient } = require('mongodb');
-const dotenv = require('dotenv');
-dotenv.config();
+const { MongoClient } = require('mongodb')
+const dotenv = require('dotenv')
+dotenv.config()
 const env = process.env
 
 class MongoDBConnector {
-  constructor() {
-    this.uri = `mongodb+srv://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}@bs3206.7ikwc7d.mongodb.net/?retryWrites=true&w=majority`;
-    this.dbName = "AdvancedSoftwareDev";
-    this.client = new MongoClient(this.uri, { useUnifiedTopology: true });
+  constructor () {
+    this.uri = `mongodb+srv://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}@bs3206.7ikwc7d.mongodb.net/?retryWrites=true&w=majority`
+    this.dbName = 'AdvancedSoftwareDev'
+    this.client = new MongoClient(this.uri, { useUnifiedTopology: true })
 
     this.connect()
   }
 
-  async connect() {
+  async connect () {
     try {
-      await this.client.connect();
-      console.log('Connected to MongoDB');
-      this.db = this.client.db(this.dbName);
+      await this.client.connect()
+      console.log('Connected to MongoDB')
+      this.db = this.client.db(this.dbName)
     } catch (error) {
-      console.error('Error connecting to MongoDB:', error);
+      console.error('Error connecting to MongoDB:', error)
     }
   }
 
-  async disconnect() {
+  async disconnect () {
     try {
-      await this.client.close();
-      console.log('Disconnected from MongoDB');
+      await this.client.close()
+      console.log('Disconnected from MongoDB')
     } catch (error) {
-      console.error('Error disconnecting from MongoDB:', error);
+      console.error('Error disconnecting from MongoDB:', error)
     }
   }
 
-  queryCollection(collectionName, query) {
+  queryCollection (collectionName, query) {
     const asyncFunction = async (collectionName, query) => {
-        try {
-            const collection = this.db.collection(collectionName);
-            const result = collection.find(query).toArray();
-            return await result;
-        } catch (error) {
-            console.error('Error querying MongoDB:', error);
-            throw error;
-        }
-    };
+      try {
+        const collection = this.db.collection(collectionName)
+        const result = collection.find(query).toArray()
+        return await result
+      } catch (error) {
+        console.error('Error querying MongoDB:', error)
+        throw error
+      }
+    }
 
-    return asyncFunction(collectionName, query);
+    return asyncFunction(collectionName, query)
+  }
 }
 
-
-}
-
-module.exports = MongoDBConnector;
-
+module.exports = MongoDBConnector
