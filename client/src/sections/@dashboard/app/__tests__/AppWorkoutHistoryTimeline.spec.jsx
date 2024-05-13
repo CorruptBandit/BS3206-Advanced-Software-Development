da716268fa271e09a7ca3adbda2d2fd6887f2702
Cabin320 <jacob.hogarty@yahoo.com>
2024-05-13 23:06:17 +0100

    adding placeholders for other component tests

---
+++ client/src/sections/@dashboard/app/__tests__/AppWorkoutHistoryTimeline.spec.jsx
@@ -0,0 +1,22 @@
+import {describe, it, expect, vi, beforeEach} from 'vitest';
+import {render, fireEvent, waitFor, findByText} from '@testing-library/react';
+import {useAuth} from '../../../../context/AuthContext';
+import {AppWorkoutHistoryTimeline} from "../index";
+import '@testing-library/jest-dom';
+
+vi.mock('../../../../context/AuthContext', () => ({
+    useAuth: vi.fn()
+}));
+
+
+describe('AppGoals Component', () => {
+    beforeEach(() => {
+        console.error = vi.fn();
+        vi.clearAllMocks();
+    });
+
+    useAuth.mockImplementation(() => ({
+        isLoggedIn: true, email: 'dave@gmail.com'
+    }));
+
+})
